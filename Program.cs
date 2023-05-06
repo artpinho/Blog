0817213d6b163c891cf05183e561e3b75150e4d4
Artenir Pinho <77869098+artpinho@users.noreply.github.com>
2023-05-05 21:07:46 -0300

    Add files via upload
    Criação dos models e repositórios de User e Roles, Connection String, Integração com o SQL via docker.

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Blog.Models;
+using Blog.Repositories;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = @"
+                                                    Server=localhost,1433;
+                                                    Database=Blog;
+                                                    User ID=sa;
+                                                    Password=1q2w3e4r@#$;
+                                                    Trusted_Connection=False; 
+                                                    TrustServerCertificate=True;";
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+
+            ReadUsers(connection);
+            ReadRoles(connection);
+            //ReadUser();
+            //CreateUser();
+            //UpdateUser();
+            //DeleteUser();
+
+            connection.Close();
+        }
+        public static void ReadUsers(SqlConnection connection)
+        {
+            var repository = new UserRepository(connection);
+            var users = repository.Get();
+
+            foreach(var user in users)
+                Console.WriteLine(user.Name);
+        }
+
+        public static void ReadRoles(SqlConnection connection)
+        {
+            var repository = new RoleRepository(connection);
+            var roles = repository.Get();
+
+            foreach(var role in roles)
+                Console.WriteLine(role.Name);
+        }
+        /*public static void ReadUser()
+        {
+            using(var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(1);
+                Console.WriteLine(user.Name);
+            }
+        }
+        public static void CreateUser()
+        {
+            var user = new User()
+            {
+                Bio="Equipe Pinho",
+                Email="equipe@pinho.com",
+                Image="https://...",
+                Name="Equipe Pinho",
+                PasswordHash="HASH2",
+                Slug="equipe-pinho"
+
+            };
+            using(var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Insert<User>(user);
+                Console.WriteLine("Cadastro realizado com sucesso");
+            }
+        }     
+        public static void UpdateUser()
+        {
+            var user = new User()
+            {
+                Id=2,
+                Bio="Equipe Artenir Pinho",
+                Email="equipe@pinho.com",
+                Image="https://...",
+                Name="Equipe de Suporte Pinho",
+                PasswordHash="HASH2",
+                Slug="equipe-pinho"
+
+            };
+            using(var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Update<User>(user);
+                Console.WriteLine("Atualização realizado com sucesso");
+            }
+        }  
+        public static void DeleteUser()
+        {
+            using(var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(2);
+                connection.Delete<User>(user);
+                Console.WriteLine("Exclusão realizada com sucesso");
+            }
+        }  */
+    }
+}
---
+++ Repositories/RoleRepository.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog.Repositories
+{
+    public class RoleRepository
+    {
+        private readonly SqlConnection _connection;
+
+        public RoleRepository(SqlConnection connection)
+            => _connection = connection;
+        
+        public IEnumerable<Role> Get()
+            => _connection.GetAll<Role>();
+
+        public Role Get(int id)
+            => _connection.Get<Role>(id);
+
+        public void Create(Role role)
+            => _connection.Insert<Role>(role);
+    }
+}
